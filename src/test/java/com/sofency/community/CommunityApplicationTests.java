d8f5a13355cc202988b1a0a60e94938223293f63
sofency <sofency@qq.com>
2020-02-22 21:45:28 +0800

    init resposity

---
+++ src/test/java/com/sofency/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofency.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
